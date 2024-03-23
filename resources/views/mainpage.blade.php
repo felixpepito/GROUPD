97f29f5d3913bb5dca9a3b5852649a7faf7635ea
felixpepito <pepitofelix980@gmail.com>
2024-03-23 12:03:51 +0800

    last commit

--- resources/views/mainpage.blade.php
+++ resources/views/mainpage.blade.php
@@ -4,91 +4,91 @@
  @section('content')
     <div class="menu-container">
         <div class="menu-item">
-            <img class="fried-chicken" src="./assets/fried-chicken.png" alt="Fried Chicken">
-            <p>Fried Chicken</p>
-            <p class="php">₱ 25</p>
+            <img class="fried-chicken" src="{{ asset('img/manok-1-CGd.png') }}" alt="Fried Chicken">
+            <p>Fried Chicken</p><br>
+            <p class="php">₱ 100</p>
 
         </div>
 
         <div class="menu-item">
-          <img class="gatorade" src="./assets/gatorade.png" alt="Gatorade">
-          <p>Gatorade</p>
-          <p class="php">₱ 40</p>
+          <img class="Sweet Spaghetti" src="{{ asset('img/pancit-1-MPs.png') }}" alt="Sweet Spaghetti">
+          <p>Sweet Spaghetti</p><br>
+          <p class="php">₱ 100</p>
       </div>
 
         <div class="menu-item">
-            <img class="coke-mismo" src="./assets/coke-mismo.png" alt="Coke Mismo">
-            <p>Coke Mismo</p>
-            <p class="php">₱ 25</p>
-        </div>
-
-        <div class="menu-item">
-          <img class="gatorade" src="./assets/gatorade.png" alt="Gatorade">
-          <p>Gatorade</p>
-          <p class="php">₱ 40</p>
-      </div>
-
-      <div class="menu-item">
-        <img class="gatorade" src="./assets/gatorade.png" alt="Gatorade">
-        <p>Gatorade</p>
-        <p class="php">₱ 40</p>
-    </div>
-
-        <div class="menu-item">
-            <img class="sweet-spaghetti" src="./assets/sweet-spaghetti.png" alt="Sweet Spaghetti">
-            <p>Sweet Spaghetti</p>
+            <img class="Pater Meal" src="{{ asset('img/rice-ang-subak-kay-dahon-sa-saging-1-cNq.png') }}" alt="Pater Meal">
+            <p> Pater Meal</p><br>
             <p class="php">₱ 50</p>
         </div>
 
         <div class="menu-item">
-          <img class="gatorade" src="./assets/gatorade.png" alt="Gatorade">
-          <p>Gatorade</p>
-          <p class="php">₱ 40</p>
+          <img class="Pancit Guisado" src="{{ asset('img/-Y5K.png') }}" alt="Pancit Guisado">
+          <p>Pancit Guisado</p><br>
+          <p class="php">₱ 80</p>
       </div>
+
       <div class="menu-item">
-        <img class="gatorade" src="./assets/gatorade.png" alt="Gatorade">
-        <p>Gatorade</p>
+        <img class="Fish Fillet" src="{{ asset('img/-UGV.png') }}" alt="Fish Fillet">
+        <p>Fish Fillet</p><br>
         <p class="php">₱ 40</p>
     </div>
+
         <div class="menu-item">
-            <img class="pater-meal" src="./assets/pater-meal.png" alt="Pater Meal">
-            <p>Pater Meal</p>
-            <p class="php">₱ 45</p>
+            <img class="Coke Mismo" src="{{ asset('img/-j8h.png') }}" alt="Coke Mismo">
+            <p>Coke Mismo</p><br>
+            <p class="php">₱ 25</p>
+        </div>
+
+        <div class="menu-item">
+          <img class="Sprite Mismo" src="{{ asset('img/image-1.png') }}" alt="Sprite Mismo">
+          <p>Sprite Mismo</p><br>
+          <p class="php">₱ 25</p>
+      </div>
+      <div class="menu-item">
+        <img class="Bottled Water" src="{{ asset('img/-j65.png') }}" alt="Bottled Water">
+        <p>Bottled Water</p><br>
+        <p class="php">₱ 15 </p>
+    </div>
+        <div class="menu-item">
+            <img class="Mountain Dew" src="{{ asset('img/image-2.png') }}" alt="Mountain Dew">
+            <p>Mountain Dew</p><br>
+            <p class="php">₱ 25</p>
         </div>
         <div class="menu-item">
-            <img class="pancit-guisado" src="./assets/pancit-guisado.png" alt="Pancit Guisado">
-            <p>Pancit Guisado</p>
+            <img class="Gatorade" src="{{ asset('img/-YXF.png') }}" alt="Gatorade">
+            <p>Gatorade</p><br>
             <p class="php">₱ 40</p>
         </div>
 		
         <div class="menu-item">
-            <img class="fish-fille" src="./assets/fish-fille.png" alt="Fish Fille">
-            <p>Fish Fille</p>
-            <p class="php">₱ 60</p>
+            <img class="Fries" src="{{ asset('img/-ATP.png') }}" alt="Fries">
+            <p>Fries</p><br>
+            <p class="php">₱ 25</p>
         </div>
         <div class="menu-item">
-            <img class="sprite-mismo" src="./assets/sprite-mismo.png" alt="Sprite Mismo">
-            <p>Sprite Mismo</p>
-            <p class="php">₱ 30</p>
+            <img class="Tempura" src="{{ asset('img/-43P.png') }}" alt="Tempura">
+            <p>Tempura</p><br>
+            <p class="php">₱ 25</p>
         </div>
         <div class="menu-item">
-            <img class="water-bottle" src="./assets/water-bottle.png" alt="Water Bottle">
-            <p>Water Bottle</p>
+            <img class="Siomai" src="{{ asset('img/dsadsaddad-1-hhF.png') }}" alt="Siomai">
+            <p>Siomai</p><br>
             <p class="php">₱ 15</p>
         </div>
         <div class="menu-item">
-            <img class="mountain-dew" src="./assets/mountain-dew.png" alt="Mountain Dew">
-            <p>Mountain Dew</p>
-            <p class="php">₱ 35</p>
+            <img class="Siopao" src="{{ asset('img/dasdsadasdasd-1-snu.png') }}" alt="Siopao">
+            <p>Siopao</p><br>
+            <p class="php">₱ 25</p>
         </div>
         <div class="menu-item">
-            <img class="gatorade" src="./assets/gatorade.png" alt="Gatorade">
-            <p>Gatorade</p>
-            <p class="php">₱ 40</p>
+            <img class="Cookies" src="{{ asset('img/image-3.png') }}" alt="Cookies">
+            <p>Cookies</p><br>
+            <p class="php">₱ 25</p>
+
         </div>
-  
-        </div>
-    </div>
+
+   <div>
 
 @endsection
 
