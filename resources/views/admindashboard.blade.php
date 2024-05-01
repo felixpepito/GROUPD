77eb4247bc6372a42b9ef2c24ddca13da895b14b
AndrianLuna2 <andrianluna.202200254@gmail.com>
2024-05-01 15:39:17 +0800

    pepito gwapo

--- resources/views/admindashboard.blade.php
+++ resources/views/admindashboard.blade.php
@@ -27,7 +27,7 @@
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid px-4">
-                        <h1 class="mt-4 py-5 text-decoration-none text-white ">Admin Dashboard</h1>
+                        <h1 class="mt-4 py-5 text-decoration-none text-white ">Admin dashboard</h1>
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-primary text-white mb-4">
